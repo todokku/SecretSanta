c053bbbebd904683fc3d7a37fc29778afb2dd096
jdg555666 <56042888+jdg555666@users.noreply.github.com>
2019-12-05 20:54:37 -0500

    Code for pulling data from SQL database
    Code for pulling data from SQL database. Needs to be integrated with Wiley's code for email.

---
+++ sqldataread.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect('secret_santa_participants.db')
+#first column: group_name
+#second column: first_name
+#third_column: last_name
+#fourth column: email
+cursor = conn.cursor()
+print("Sucessfully connected to database")
+
+group_names = []
+first_names = []
+last_names = []
+emails = []
+#Arrays to hold all information
+
+select_query = """SELECT * from Contacts""" #Contacts is name of table in example
+cursor.execute(select_query)
+records = cursor.fetchall() #set records to pull all data from database
+print("Total rows are: ", len(records))
+
+for row in records:
+    group_names.append(row[0]) #assuming group name is first column
+    first_names.append(row[1]) #assuming first name is secnod column
+    last_names.append(row[2]) #assuming last nme is third column
+    emails.append(row[3]) #assuming emails is last column
+
+print(first_names[0], last_names[0], emails[0])
+
+#run email bot in a loop below here using the arrays
