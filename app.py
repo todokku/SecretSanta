a907369e78dcbc098808d9747c5234e853c35d86
Ben Leone <benleone@dhcp-wifi-8021x-155-41-69-6.bu.edu>
2019-12-08 17:45:52 -0500

    added uuid column back

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ db = SQLAlchemy(app)
 class SecretSanta(db.Model):
     __tablename__ = 'secretsanta'
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(200), unique=True)
     member = db.Column(db.String(200))
     email = db.Column(db.String(200), unique=True)
     wishlist = db.Column(db.Text())
