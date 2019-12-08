7e00e963f5b44cbd50b64a6d2ed887dd4214e52c
John Hunt <johnhunt@dhcp-wifi-8021x-155-41-40-10.bu.edu>
2019-12-08 13:40:39 -0500

    Update again

--- app.py
+++ app.py
@@ -81,11 +81,11 @@ def submit():
                 if db.session.query(Feedback).filter(Feedback.email == email[ii]).count() == 0:
                     data = Feedback(member[ii], email[ii])
                     db.session.add(data)
-                    token = generate_token(email[ii])#wiley add start
+                    token = generate_token(ii['email'])#wiley add start
                     link = url_for('wishlist/<user_id>', token = token, _external = True)
                     msg = Message('Hello from Optimal Secret Santa!',#subject
                     sender = 'OptimalSecretSanta@gmail.com',
-                    recipients= [email[ii]])
+                    recipients= [ii['email'])
                     msg.body = F"Hi {member[ii]},\n\nGreetings from the North Pole!\n\nYou have been added to a Secret Santa group created on optimal-secret-santa.herokuapp.com.\n\nPlease use the below link to fill out the wishlist/message you would like to send your Secret Santa.\n\nLink:{link}\n\nHappy Holidays!\n\nSincerely,\nOptimalSecretSanta."
                     thr = Thread(target=send_thread_email, args=[msg])
                     thr.start()#wiley add end
