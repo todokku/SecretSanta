d2d3c6dc1be8628c59b9c0d037c51fdc2904826e
Ben Leone <benleone@dhcp-wifi-8021x-155-41-41-137.bu.edu>
2019-12-10 17:28:47 -0500

    Comment out validation of email for double emails

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ def submit():
         pair = generate_pairings(email)
         print(member, email, pair)
         # Checks if each email in the list is unique by comparing to a set
-        if len(email) > len(set(email)):
-            return render_template('index.html', message='A user with this email is already a part of Secret Santa')
+        # if len(email) > len(set(email)):
+        #     return render_template('index.html', message='A user with this email is already a part of Secret Santa')
         for ii in range(len(member)):
             if member[ii] == '' or email[ii] == '':
                 return render_template('index.html', message='Please ensure all fields are entered')
